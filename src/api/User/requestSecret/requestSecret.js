08ddf03c77604f733350216c7fbf4ffe9dd6f91b
yezii <1126yezy@gmail.com>
2020-12-15 20:56:17 +0900

    passport jwt

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -1,17 +1,21 @@
 import { makePrismaClientClass } from "prisma-client-lib";
 import { prisma } from "../../../../generated/prisma-client";
-import { generateSecret, secretGenerator } from "../../../utils";
+import {
+  generateSecret,
+  secretGenerator,
+  sendSecretMail,
+} from "../../../utils";
 
 export default {
   Mutation: {
     requestSecret: async (_, args) => {
       const { email } = args;
       const loginSecret = generateSecret();
-      console.log(loginSecret);
       try {
+        await sendSecretMail(email, loginSecret);
         await prisma.updateUser({ data: { loginSecret }, where: { email } });
         return true;
-      } catch (error) {
+      } catch {
         return false;
       }
     },
--- src/utils.js
+++ src/utils.js
@@ -1,10 +1,7 @@
 import { adjectives, nouns } from "./words";
 import nodemailer from "nodemailer";
 import sgTransport from "nodemailer-mailgun-transport";
-import dotenv from "dotenv";
-import path from "path";
-
-dotenv.config({ path: path.resolve(__dirname, ".env") });
+import jwt from "jsonwebtoken";
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length);
@@ -27,7 +24,9 @@ export const sendSecretMail = (address, secret) => {
     from: "1126yezy@gmail.com",
     to: address,
     subject: "🔒 Login Secret for Prismagram 🔒",
-    html: `Hello! Your login secret it ${secret}.<br/>Copy paste on the app/website to login`,
+    html: `Hello! Your secret word is <strong>${secret}</strong>.<br/>Copy paste on the app/website to login`,
   };
   return sendMail(email);
 };
+
+export const generateToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET);
