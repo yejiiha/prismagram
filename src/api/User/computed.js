059cb2df97b000b749dcefe8a87d5ffe8c8749d5
yezii <1126yezy@gmail.com>
2020-12-16 20:57:26 +0900

    Computed, isFollowing and isSelf Filed

---
+++ src/api/User/computed.js
@@ -0,0 +1,35 @@
+import { prisma } from "../../../generated/prisma-client";
+
+export default {
+  User: {
+    fullName: (parent) => {
+      return `${parent.firstName} ${parent.lastName}`;
+    },
+    isFollowing: async (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      try {
+        return prisma.$exists.user({
+          AND: [
+            {
+              id: user.id,
+            },
+            {
+              following_some: {
+                id: parentId,
+              },
+            },
+          ],
+        });
+      } catch {
+        return false;
+      }
+    },
+    // 요청하는 사람(parent)과 요청받는 사람(request)이 같으면 내 프로필을 요청함
+    isSelf: (parent, _, { request }) => {
+      const { user } = request;
+      const { id: parentId } = parent;
+      return user.id === parentId;
+    },
+  },
+};
