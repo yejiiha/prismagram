1b5dc3c16ff9d5c6fa9959b3f1bc462dd1082960
yezii <1126yezy@gmail.com>
2020-12-28 16:19:54 +0900

    Add default avatar on User and Add commentCount on Post

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -6,7 +6,7 @@ export default {
     comments: ({ id }) => prisma.post({ id }).comments(),
     user: ({ id }) => prisma.post({ id }).user(),
     likes: ({ id }) => prisma.post({ id }).likes(),
-    isLiked: async (parent, _, { request }) => {
+    isLiked: (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
       return prisma.$exists.like({
@@ -27,6 +27,13 @@ export default {
     likeCount: (parent) =>
       prisma
         .likesConnection({
+          where: { post: { id: parent.id } },
+        })
+        .aggregate()
+        .count(),
+    commentCount: (parent) =>
+      prisma
+        .commentsConnection({
           where: { post: { id: parent.id } },
         })
         .aggregate()
